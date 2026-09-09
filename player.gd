327ed6a36be35286e1ba32e17cec397673035aa8
Danilo Volonte <volonte.danilo.06@itisdalmine.edu.it>
2026-09-09 12:45:13 +0200

    fatto avanti e indietro con accelerazione e franata

--- player.gd
+++ player.gd
@@ -1,29 +1,29 @@
 extends CharacterBody2D
 
-
 @export var speed = 400
+@export var rotation_speed = 1.5
+@export var acceleration := 200.0
+@export var brake := 300.0
+@export var max_speed := 1000.0
 
+var rotationDirection = 0
 
 func _physics_process(delta: float) -> void:
-	if(Input.is_action_pressed("left")):
-		position.x -= 1
-		
-	if(Input.is_action_pressed("right")):
-		position.x += 1
-		
-	if(Input.is_action_pressed("up")):
-		position.y -= 1
-		
-	if(Input.is_action_pressed("down")):
-		position.y += 1
-		
-		
+
+	if Input.is_action_pressed("up"):
+		velocity -= transform.y * acceleration * delta
+
+	if Input.is_action_pressed("down"):
+		velocity += transform.y * brake * delta
+
+	velocity = velocity.limit_length(max_speed)
+
 	get_input()
-	#rotation += rotation_direction * rotation_speed * delta
+
+	rotation += rotationDirection * rotation_speed * delta
+
 	move_and_slide()
-	
 
 
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+	var input_direction = Input.get_axis("left", "right")
