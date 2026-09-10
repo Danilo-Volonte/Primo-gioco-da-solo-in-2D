94b54d25eccfc6b16e44980aa2e2c0eacd09bd13
Danilo Volonte <volonte.danilo.06@itisdalmine.edu.it>
2026-09-10 13:06:22 +0200

    fatto freno motore

--- player.gd
+++ player.gd
@@ -18,7 +18,11 @@ func _physics_process(delta: float) -> void:
 
 	if Input.is_action_pressed("down"):
 		velocity += transform.y * brake * delta
-
+	
+	if(!Input.is_action_pressed("down") and !Input.is_action_pressed("up")):
+		velocity = velocity.move_toward(Vector2.ZERO, engineBraking * delta)
+		
+		
 	velocity = velocity.limit_length(max_speed)
 
 	get_input()
@@ -27,7 +31,7 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 	
-	contachilometri.text = str(int(velocity.length())) + " Km/h"
+	contachilometri.text = str(int(velocity.length()/100)) + " Km/h"
 
 func get_input():
-	var input_direction = Input.get_axis("left", "right")
+	Input.get_axis("left", "right")
